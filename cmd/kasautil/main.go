f6d5ade7aeddf16c005aceba46941ece40890295
Christian Funkhouser <christian@funkhouse.rs>
2021-05-06 15:39:16 -0400

    Test the formatters

--- cmd/kasautil/main.go
+++ cmd/kasautil/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"os"
@@ -15,7 +14,7 @@ import (
 	"github.com/cfunkhouser/kasa/export"
 )
 
-// Version of kasautil.
+// Version of kasautil. Set at build time to something meaningful.
 var Version = "development"
 
 var (
@@ -27,37 +26,6 @@ var (
 		},
 	})
 )
-
-func parseAddrs(c *cli.Context) (daddr, laddr *net.UDPAddr, err error) {
-	daddr, err = kasa.ParseAddr(c.String("device"))
-	if err != nil {
-		return
-	}
-	if l := c.String("local"); l != "" {
-		if laddr, err = kasa.ParseAddr(l); err != nil {
-			return
-		}
-	}
-	return
-}
-
-func parseFormatter(c *cli.Context) (formatter, error) {
-	f := c.String("format")
-	switch f {
-	case "promsd":
-		return promFileSD, nil
-	case "", "human":
-		return human, nil
-	}
-	return human, fmt.Errorf("unsupported format %q", f)
-}
-
-func parseOutFile(c *cli.Context) (io.Writer, error) {
-	if o := c.String("output"); o != "" {
-		return os.Create(o)
-	}
-	return os.Stdout, nil
-}
 
 func setState(c *cli.Context, state bool) error {
 	daddr, laddr, err := parseAddrs(c)
